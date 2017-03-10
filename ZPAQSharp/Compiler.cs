d29ca20696e4db599e923e6eb53088482933bbd4
Matt Nadareski <mnadareski@outlook.com>
2017-03-09 22:55:17 -0800

    Bring over last of c++ code; begin converting

--- ZPAQSharp/Compiler.cs
+++ ZPAQSharp/Compiler.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-using U8 = System.Byte;
-using U16 = System.UInt16;
-using U32 = System.UInt32;
-using U64 = System.UInt64;
 
 namespace ZPAQSharp
 {
@@ -18,7 +10,7 @@ namespace ZPAQSharp
 		// Compile a configuration file. Store COMP/HCOMP section in hcomp.
 		// If there is a PCOMP section, store it in pcomp and store the PCOMP
 		// command in pcomp_cmd. Replace "$1..$9+n" with args[0..8]+n
-		public Compiler(string @in, int[] args, ZPAQL hz, ZPAQL pz, Writer out2)
+		public Compiler(char[] @in, int[] args, ZPAQL hz, ZPAQL pz, Writer out2)
 		{
 			this.@in = @in;
 			this.args = args;
@@ -35,44 +27,46 @@ namespace ZPAQSharp
 			hz.header.resize(68000);
 
 			// Compile the COMP section of header
-			rtoken("comp");
-			hz.header[2] = rtoken(0, 255);  // hh
-			hz.header[3] = rtoken(0, 255);  // hm
-			hz.header[4] = rtoken(0, 255);  // ph
-			hz.header[5] = rtoken(0, 255);  // pm
-			const int n = hz.header[6] = rtoken(0, 255);  // n
+			RToken("comp");
+			hz.header[2] = (byte)RToken(0, 255);  // hh
+			hz.header[3] = (byte)RToken(0, 255);  // hm
+			hz.header[4] = (byte)RToken(0, 255);  // ph
+			hz.header[5] = (byte)RToken(0, 255);  // pm
+			int n = hz.header[6] = (byte)RToken(0, 255);  // n
 			hz.cend = 7;
 			for (int i = 0; i < n; ++i)
 			{
-				rtoken(i, i);
-				CompType type = CompType(rtoken(compname));
-				hz.header[hz.cend++] = type;
-				int clen = libzpaq::compsize[type & 255];
-				if (clen < 1 || clen > 10) syntaxError("invalid component");
+				RToken(i, i);
+				CompType type = (CompType)RToken(compname);
+				hz.header[(ulong)hz.cend++] = (byte)type;
+				int clen = LibZPAQ.compsize[(int)type & 255];
+				if (clen < 1 || clen > 10) SyntaxError("invalid component");
 				for (int j = 1; j < clen; ++j)
-					hz.header[hz.cend++] = rtoken(0, 255);  // component arguments
+				{
+					hz.header[(ulong)hz.cend++] = (byte)RToken(0, 255);  // component arguments
+				}
 			}
-			hz.header[hz.cend++];  // end
+			hz.cend++;  // end
 			hz.hbegin = hz.hend = hz.cend + 128;
 
 			// Compile HCOMP
-			rtoken("hcomp");
-			int op = compile_comp(hz);
+			RToken("hcomp");
+			int op = CompileComp(hz);
 
 			// Compute header size
 			int hsize = hz.cend - 2 + hz.hend - hz.hbegin;
-			hz.header[0] = hsize & 255;
-			hz.header[1] = hsize >> 8;
+			hz.header[0] = (byte)(hsize & 255);
+			hz.header[1] = (byte)(hsize >> 8);
 
 			// Compile POST 0 END
-			if (op == POST)
+			if (op == (int)CompType.POST)
 			{
-				rtoken(0, 0);
-				rtoken("end");
+				RToken(0, 0);
+				RToken("end");
 			}
 
 			// Compile PCOMP pcomp_cmd ; program... END
-			else if (op == PCOMP)
+			else if (op == (int)CompType.PCOMP)
 			{
 				pz.header.resize(68000);
 				pz.header[4] = hz.header[4];  // ph
@@ -81,28 +75,43 @@ namespace ZPAQSharp
 				pz.hbegin = pz.hend = pz.cend + 128;
 
 				// get pcomp_cmd ending with ";" (case sensitive)
-				next();
-				while (*in && *in!= ';') {
-					if (out2)
-						out2->put(*in);
-					++in;
+				Next();
+				while (@in[inptr] != '\0' && @in[inptr] != ';')
+				{
+					if (out2 != null)
+					{
+						out2.put(@in[inptr]);
+					}
+					++inptr;
 				}
-				if (*in) ++in;
+				if (@in != null)
+				{
+					++inptr;
+				}
 
 				// Compile PCOMP
-				op = compile_comp(pz);
+				op = CompileComp(pz);
 				int len = pz.cend - 2 + pz.hend - pz.hbegin;  // insert header size
-				assert(len >= 0);
-				pz.header[0] = len & 255;
-				pz.header[1] = len >> 8;
-				if (op != END)
-					syntaxError("expected END");
+
+				if (len < 0)
+				{
+					throw new Exception("Header size must be greater than 0");
+				}
+				pz.header[0] = (byte)(len & 255);
+				pz.header[1] = (byte)(len >> 8);
+				if (op != (int)CompType.END)
+				{
+					SyntaxError("expected END");
+				}
 			}
-			else if (op != END)
-				syntaxError("expected END or POST 0 END or PCOMP cmd ; ... END");
+			else if (op != (int)CompType.END)
+			{
+				SyntaxError("expected END or POST 0 END or PCOMP cmd ; ... END");
+			}
 		}
 
-		private string @in; // ZPAQL source code
+		private char[] @in; // ZPAQL source code
+		private int inptr; // Pointer to the array
 		private int[] args; // Array of up to 9 args, default NULL = all 0
 		private ZPAQL hz; // Output of COMP and HCOMP sections
 		private ZPAQL pz; // Output of PCOMP section
@@ -113,164 +122,286 @@ namespace ZPAQSharp
 		// Symbolic constants
 		private enum CompType
 		{
-			NONE, CONS, CM, ICM, MATCH, AVG, MIX2, MIX, ISSE, SSE,
-			JT = 39, JF = 47, JMP = 63, LJ = 255,
-			POST = 256, PCOMP, END, IF, IFNOT, ELSE, ENDIF, DO,
-			WHILE, UNTIL, FOREVER, IFL, IFNOTL, ELSEL, SEMICOLON
+			NONE,
+			CONS,
+			CM,
+			ICM,
+			MATCH,
+			AVG,
+			MIX2,
+			MIX,
+			ISSE,
+			SSE,
+			JT = 39,
+			JF = 47,
+			JMP = 63,
+			LJ = 255,
+			POST = 256,
+			PCOMP,
+			END,
+			IF,
+			IFNOT,
+			ELSE,
+			ENDIF,
+			DO,
+			WHILE,
+			UNTIL,
+			FOREVER,
+			IFL,
+			IFNOTL,
+			ELSEL,
+			SEMICOLON
 		}
 
 		// Print error message and exit
-		private void syntaxError(string msg, string expected = null) // error()
+		// TODO: Work on commented code
+		private void SyntaxError(string msg, string expected = null) // error()
 		{
-			Array<char> sbuf(128);  // error message to report
-			char* s = &sbuf[0];
-			strcat(s, "Config line ");
-			for (int i = strlen(s), r = 1000000; r; r /= 10)  // append line number
-				if (line / r) s[i++] = '0' + line / r % 10;
-			strcat(s, " at ");
-			for (int i = strlen(s); i < 40 && *in> ' '; ++i)  // append token found
-    s[i] = *in++;
-			strcat(s, ": ");
-			strncat(s, msg, 40);  // append message
-			if (expected)
+			StringBuilder sbuf = new StringBuilder();
+			sbuf.Append("Config line ");
+
+			/*
+			for (int i = sbuf.Length, r = 1000000; r > 0; r /= 10) // append line number
 			{
-				strcat(s, ", expected: ");
-				strncat(s, expected, 20);  // append expected token if any
+				if (line / r > 0)
+				{
+					sbuf.Append("0", line / r % 10);
+				}
 			}
-			error(s);
+			*/
+			sbuf.Append(" at ");
+			/*
+			for (int i = sbuf.Length; i < 40 && @in[inptr] > ' '; ++i) // append token found
+			{
+				sbuf.Append(@in[inptr++]);
+			}
+			*/
+			sbuf.Append(": ");
+			sbuf.Append(msg.Substring(0, 40));
+			if (expected != null)
+			{
+				sbuf.Append(", expected: ");
+				sbuf.Append(expected.Substring(0, 20)); // append expected token if any
+			}
+			LibZPAQ.error(sbuf.ToString());
 		}
 
 		// Advance in to start of next token. Tokens are delimited by white
 		// space. Comments inclosed in ((nested) parenthsis) are skipped.
-		private void next() // advance in to next token
+		private void Next() // advance in to next token
 		{
-			assert(in);
-			for (; *in; ++in) {
-				if (*in== '\n') ++line;
-				if (*in== '(') state += 1 + (state < 0);
-    else if (state > 0 && *in== ')') --state;
-    else if (state < 0 && *in<= ' ') state = 0;
-    else if (state == 0 && *in> ' ') { state = -1; break; }
+			if (@in == null)
+			{
+				throw new Exception("Expected ZPAQL source code to be non-null");
 			}
-			if (!*in) error("unexpected end of config");
+
+			for (; @in[inptr] != '\0'; ++inptr)
+			{
+				if (@in[inptr] == '\n')
+				{
+					++line;
+				}
+				if (@in[inptr] == '(')
+				{
+					state += 1 + (state < 0 ? 1 : 0);
+				}
+				else if (state > 0 && @in[inptr] == ')')
+				{
+					--state;
+				}
+				else if (state < 0 && @in[inptr] <= ' ')
+				{
+					state = 0;
+				}
+				else if (state == 0 && @in[inptr] > ' ')
+				{
+					state = -1;
+					break;
+				}
+			}
+			if (inptr != @in.Length) // !*in
+			{
+				LibZPAQ.error("unexpected end of config");
+			}
 		}
 
 		// return true if in==word up to white space or '(', case insensitive
-		private bool matchToken(string tok) // in==token?
+		private bool MatchToken(char[] word, int wordptr) // in==token?
 		{
-			const char* a =in;
-			for (; (*a > ' ' && *a != '(' && *word); ++a, ++word)
-				if (tolower(*a) != tolower(*word)) return false;
-			return !*word && (*a <= ' ' || *a == '(');
+			int a = inptr;
+			for (; (@in[a] > ' ' && @in[a] != '(' && word[wordptr] != '\0'); ++a, ++wordptr)
+			{
+				if (tolower(@in[a]) != tolower(word[wordptr]))
+				{
+					return false;
+				}
+			}
+			return word[wordptr] != '\0' && (@in[a] <= ' ' || @in[a] == '(');
 		}
 
 		// Read a number in (low...high) or exit with an error
 		// For numbers like $N+M, return arg[N-1]+M
-		private int rtoken(int low, int high) // return token which must be in range
+		private int RToken(int low, int high) // return token which must be in range
 		{
-			next();
+			Next();
 			int r = 0;
-			if (@in[0]=='$' && @in[1]>='1' && @in[1]<='9')
+			if (@in[0] == '$' && @in[1] >= '1' && @in[1] <= '9')
 			{
-				if (@in[2]=='+') r=atoi(@in+3);
-				if (args) r+=args[@in[1]-'1'];
+				if (@in[2] == '+')
+				{
+					r = (int)@in[inptr] + 3;
+				}
+				if (args != null)
+				{
+					r += args[@in[1] - '1'];
+				}
 			}
-			else if (@in[0]=='-' || (@in[0]>='0' && @in[0]<='9')) r=atoi(@in);
-			else syntaxError("expected a number");
-			if (r<low) syntaxError("number too low");
-			if (r>high) syntaxError("number too high");
+			else if (@in[0] == '-' || (@in[0] >= '0' && @in[0] <= '9'))
+			{
+				r = (int)@in[inptr];
+			}
+			else
+			{
+				SyntaxError("expected a number");
+			}
+			if (r < low)
+			{
+				SyntaxError("number too low");
+			}
+			if (r > high)
+			{
+				SyntaxError("number too high");
+			}
 			return r;
 		}
 
 		// Read a token which must be the specified value s
-		private void rtoken(string s) // return toke which must be s
+		private void RToken(string s) // return token which must be s
 		{
-			assert(s);
-			next();
-			if (!matchToken(s)) syntaxError("expected", s);
+			if (s != null)
+			{
+				throw new ArgumentNullException(nameof(s));
+			}
+			Next();
+			if (!MatchToken(s.ToCharArray(), 0))
+			{
+				SyntaxError("expected", s.ToString());
+			}
 		}
 
 		// Read a token, which must be in the NULL terminated list or else
 		// exit with an error. If found, return its index.
-		private void rtoken(string[] list)
+		private int RToken(string[] list)
 		{
-			assert(in);
-			assert(list);
-			next();
-			for (int i = 0; list[i]; ++i)
-				if (matchToken(list[i]))
+			if (@in == null)
+			{
+				throw new Exception("Expected ZPAQL source code to be non-null");
+			}
+			if (list == null)
+			{
+				throw new ArgumentNullException(nameof(list));
+			}
+			Next();
+			for (int i = 0; list[i] != null; ++i)
+			{
+				if (MatchToken(list[i].ToCharArray(), 0))
+				{
 					return i;
-			syntaxError("unexpected");
-			assert(0);
-			return -1; // not reached
+				}
+			}
+
+			SyntaxError("unexpected");
+			throw new Exception("Unexpected end of source code");
 		}
 
 		// Compile HCOMP or PCOMP code. Exit on error. Return
 		// code for end token (POST, PCOMP, END)
-		private int compile_comp(ZPAQL z) // compile either HCOMP or PCOMP
+		private int CompileComp(ZPAQL z) // compile either HCOMP or PCOMP
 		{
 			int op = 0;
-			const int comp_begin = z.hend;
+			int comp_begin = z.hend;
 			while (true)
 			{
-				op = rtoken(opcodelist);
-				if (op == POST || op == PCOMP || op == END) break;
+				op = RToken(opcodelist);
+				if (op == (int)CompType.POST || op == (int)CompType.PCOMP || op == (int)CompType.END)
+				{
+					break;
+				}
+
 				int operand = -1; // 0...255 if 2 bytes
 				int operand2 = -1;  // 0...255 if 3 bytes
-				if (op == IF)
+				if (op == (int)CompType.IF)
 				{
-					op = JF;
+					op = (int)CompType.JF;
 					operand = 0; // set later
-					if_stack.push(z.hend + 1); // save jump target location
+					if_stack.push((ushort)(z.hend + 1)); // save jump target location
 				}
-				else if (op == IFNOT)
+				else if (op == (int)CompType.IFNOT)
 				{
-					op = JT;
+					op = (int)CompType.JT;
 					operand = 0;
-					if_stack.push(z.hend + 1); // save jump target location
+					if_stack.push((ushort)(z.hend + 1)); // save jump target location
 				}
-				else if (op == IFL || op == IFNOTL)
-				{  // long if
-					if (op == IFL) z.header[z.hend++] = (JT);
-					if (op == IFNOTL) z.header[z.hend++] = (JF);
-					z.header[z.hend++] = (3);
-					op = LJ;
-					operand = operand2 = 0;
-					if_stack.push(z.hend + 1);
-				}
-				else if (op == ELSE || op == ELSEL)
+				else if (op == (int)CompType.IFL || op == (int)CompType.IFNOTL) // long if
 				{
-					if (op == ELSE) op = JMP, operand = 0;
-					if (op == ELSEL) op = LJ, operand = operand2 = 0;
+					if (op == (int)CompType.IFL)
+					{
+						z.header[z.hend++] = (int)CompType.JT;
+					}
+					if (op == (int)CompType.IFNOTL)
+					{
+						z.header[z.hend++] = (int)CompType.JF;
+					}
+					z.header[z.hend++] = (3);
+					op = (int)CompType.LJ;
+					operand = operand2 = 0;
+					if_stack.push((ushort)(z.hend + 1));
+				}
+				else if (op == (int)CompType.ELSE || op == (int)CompType.ELSEL)
+				{
+					if (op == (int)CompType.ELSE)
+					{
+						op = (int)CompType.JMP;
+						operand = 0;
+					}
+					if (op == (int)CompType.ELSEL)
+					{
+						op = (int)CompType.LJ;
+						operand = operand2 = 0;
+					}
 					int a = if_stack.pop();  // conditional jump target location
-					assert(a > comp_begin && a < int(z.hend));
-					if (z.header[a - 1] != LJ)
-					{  // IF, IFNOT
-						assert(z.header[a - 1] == JT || z.header[a - 1] == JF || z.header[a - 1] == JMP);
-						int j = z.hend - a + 1 + (op == LJ); // offset at IF
+					if (a <= comp_begin || a >= z.hend)
+					{
+						throw new Exception("Expected target location to be within the bounds");
+					}
+					if (z.header[a - 1] != (int)CompType.LJ) // IF, IFNOT
+					{
+						// TODO: Ended here
+						assert(z.header[a - 1] == (int)CompType.JT || z.header[a - 1] == (int)CompType.JF || z.header[a - 1] == (int)CompType.JMP);
+						int j = z.hend - a + 1 + (op == (int)CompType.LJ ? 1 : 0); // offset at IF
 						assert(j >= 0);
-						if (j > 127) syntaxError("IF too big, try IFL, IFNOTL");
+						if (j > 127) SyntaxError("IF too big, try IFL, IFNOTL");
 						z.header[a] = j;
 					}
 					else
 					{  // IFL, IFNOTL
-						int j = z.hend - comp_begin + 2 + (op == LJ);
+						int j = z.hend - comp_begin + 2 + (op == (int)CompType.LJ ? 1 : 0);
 						assert(j >= 0);
 						z.header[a] = j & 255;
 						z.header[a + 1] = (j >> 8) & 255;
 					}
-					if_stack.push(z.hend + 1);  // save JMP target location
+					if_stack.push((ushort)(z.hend + 1));  // save JMP target location
 				}
-				else if (op == ENDIF)
+				else if (op == (int)CompType.ENDIF)
 				{
 					int a = if_stack.pop();  // jump target address
 					assert(a > comp_begin && a < int(z.hend));
 					int j = z.hend - a - 1;  // jump offset
 					assert(j >= 0);
-					if (z.header[a - 1] != LJ)
+					if (z.header[a - 1] != (int)CompType.LJ)
 					{
-						assert(z.header[a - 1] == JT || z.header[a - 1] == JF || z.header[a - 1] == JMP);
-						if (j > 127) syntaxError("IF too big, try IFL, IFNOTL, ELSEL\n");
+						assert(z.header[a - 1] == (int)CompType.JT || z.header[a - 1] == (int)CompType.JF || z.header[a - 1] == (int)CompType.JMP);
+						if (j > 127) SyntaxError("IF too big, try IFL, IFNOTL, ELSEL\n");
 						z.header[a] = j;
 					}
 					else
@@ -281,11 +412,11 @@ namespace ZPAQSharp
 						z.header[a + 1] = (j >> 8) & 255;
 					}
 				}
-				else if (op == DO)
+				else if (op == (int)CompType.DO)
 				{
-					do_stack.push(z.hend);
+					do_stack.push((ushort)z.hend);
 				}
-				else if (op == WHILE || op == UNTIL || op == FOREVER)
+				else if (op == (int)CompType.WHILE || op == (int)CompType.UNTIL || op == (int)CompType.FOREVER)
 				{
 					int a = do_stack.pop();
 					assert(a >= comp_begin && a < int(z.hend));
@@ -293,45 +424,45 @@ namespace ZPAQSharp
 					assert(j <= -2);
 					if (j >= -127)
 					{  // backward short jump
-						if (op == WHILE) op = JT;
-						if (op == UNTIL) op = JF;
-						if (op == FOREVER) op = JMP;
+						if (op == (int)CompType.WHILE) op = (int)CompType.JT;
+						if (op == (int)CompType.UNTIL) op = (int)CompType.JF;
+						if (op == (int)CompType.FOREVER) op = (int)CompType.JMP;
 						operand = j & 255;
 					}
 					else
 					{  // backward long jump
 						j = a - comp_begin;
 						assert(j >= 0 && j < int(z.hend) - comp_begin);
-						if (op == WHILE)
+						if (op == (int)CompType.WHILE)
 						{
-							z.header[z.hend++] = (JF);
+							z.header[z.hend++] = ((int)CompType.JF);
 							z.header[z.hend++] = (3);
 						}
-						if (op == UNTIL)
+						if (op == (int)CompType.UNTIL)
 						{
-							z.header[z.hend++] = (JT);
+							z.header[z.hend++] = ((int)CompType.JT);
 							z.header[z.hend++] = (3);
 						}
-						op = LJ;
+						op = (int)CompType.LJ;
 						operand = j & 255;
 						operand2 = j >> 8;
 					}
 				}
 				else if ((op & 7) == 7)
 				{ // 2 byte operand, read N
-					if (op == LJ)
+					if (op == (int)CompType.LJ)
 					{
-						operand = rtoken(0, 65535);
+						operand = RToken(0, 65535);
 						operand2 = operand >> 8;
 						operand &= 255;
 					}
-					else if (op == JT || op == JF || op == JMP)
+					else if (op == (int)CompType.JT || op == (int)CompType.JF || op == (int)CompType.JMP)
 					{
-						operand = rtoken(-128, 127);
+						operand = RToken(-128, 127);
 						operand &= 255;
 					}
 					else
-						operand = rtoken(0, 255);
+						operand = RToken(0, 255);
 				}
 				if (op >= 0 && op <= 255)
 					z.header[z.hend++] = (op);
@@ -340,7 +471,7 @@ namespace ZPAQSharp
 				if (operand2 >= 0)
 					z.header[z.hend++] = (operand2);
 				if (z.hend >= z.header.isize() - 130 || z.hend - z.hbegin + z.cend - 2 > 65535)
-					syntaxError("program too big");
+					SyntaxError("program too big");
 			}
 			z.header[z.hend++] = (0); // END
 			return op;
@@ -349,16 +480,16 @@ namespace ZPAQSharp
 		// Stack of n elements
 		private class Stack
 		{
-			Array<U16> s;
+			Array<ushort> s;
 			ulong top;
 
 			public Stack(int n)
 			{
-				s = new Array<U16>((ulong)n);
+				s = new Array<ushort>((ulong)n);
 				top = 0;
 			}
 
-			public void push(U16 x)
+			public void push(ushort x)
 			{
 				if (top >= s.size())
 				{
@@ -368,7 +499,7 @@ namespace ZPAQSharp
 				s[top++] = x;
 			}
 
-			public U16 pop()
+			public ushort pop()
 			{
 				if (top <= 0)
 				{
